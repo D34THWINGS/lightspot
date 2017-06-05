982c7b9cdb887024549b4e8c4d952f50ef7de6d7
d34thwings <d34thwings@gmail.com>
2017-06-05 12:10:45 +0000

    feat(calculator): Display basic calculus result

--- src/app/modules/calculator/services/calculator.service.ts
+++ src/app/modules/calculator/services/calculator.service.ts
@@ -7,13 +7,22 @@ import { IResult } from '../../results/interfaces/result.interface';
 @Injectable()
 export class CalculatorService {
   private $results: Observable<IResult[]>;
+  private calculusMatcher = /^[0-9]+(?:\s*[-+\/*%]\s*[0-9]+)+$/;
 
   constructor(searchInputService: SearchInputService) {
     this.$results = searchInputService.getInputObservable()
-      .map((value: string): IResult[] => value.length === 0 ? [] : [{
-        title: `xX_${value}_Xx`,
-      }])
-      .startWith([]);
+      .map((value: string): IResult[] => {
+        if (value.length === 0 || !this.calculusMatcher.test(value)) {
+          return [];
+        }
+
+        return [{
+          // tslint:disable-next-line:no-eval
+          title: eval(value),
+        }];
+      })
+      .startWith([])
+      .share();
   }
 
   public getResultsObservable() {
--- src/app/modules/results/services/results.service.ts
+++ src/app/modules/results/services/results.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
 import { CalculatorService } from '../../calculator/services/calculator.service';
 import { IResult } from '../interfaces/result.interface';
@@ -9,9 +10,27 @@ export class ResultsService {
   private $results: Observable<IResult[]>;
 
   constructor(calculatorService: CalculatorService) {
+    const resultsObservables = {
+      calc: calculatorService.getResultsObservable(),
+    };
+
     this.$results = Observable
-      .merge(calculatorService.getResultsObservable())
-      .startWith([]);
+      .create((subscriber: Subscriber<object>) => {
+        const results = {};
+
+        Object.keys(resultsObservables).forEach((key: string) => {
+          resultsObservables[key].subscribe(
+            (r: IResult[]) => {
+              results[key] = r;
+              subscriber.next([].concat(...Object.keys(results).map((k: string): IResult[] => results[key])));
+            },
+            (err: Error) => subscriber.error(err),
+            () => subscriber.complete(),
+          );
+        });
+      })
+      .startWith([])
+      .share();
   }
 
   public getResultsObservable() {
